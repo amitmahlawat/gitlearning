c499f1b0dd3b7103cdb2da1c70264bbe63b04465
amitmahlawat <amitmahlawat3@gmail.com>
2022-06-26 09:02:58 +0530

    getelementbtagname

--- demo.js
+++ demo.js
@@ -35,14 +35,25 @@ var headerTitle = document.getElementById('header-title')
 
 // Getelement by class name
 
-var items = document.getElementsByClassName("list-group-item");
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'Hello 2'
-items[1].style.fontWeight = 'bold'
-items[2].style.backgroundColor = 'green'
+// var items = document.getElementsByClassName("list-group-item");
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello 2'
+// items[1].style.fontWeight = 'bold'
+// items[2].style.backgroundColor = 'green'
 
-for( var i = 0; i<items.length; i++){
-items[i].style.fontWeight = "bold";
+// for( var i = 0; i<items.length; i++){
+// items[i].style.fontWeight = "bold";
+
+// }
+var li = document.getElementsByTagName("li");
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2'
+li[1].style.fontWeight = 'bold'
+li[2].style.backgroundColor = 'green'
+
+for( var i = 0; i<li.length; i++){
+li[i].style.fontWeight = "bold";
 
 }
